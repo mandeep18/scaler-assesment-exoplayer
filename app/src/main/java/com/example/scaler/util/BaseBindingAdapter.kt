753a4c101f72e7e04f79e9847dd723786c85de5f
mandeep18 <ch.mandeep18@gmail.com>
2022-06-12 20:04:33 +0530

    bug fixes

--- app/src/main/java/com/example/scaler/util/BaseBindingAdapter.kt
+++ app/src/main/java/com/example/scaler/util/BaseBindingAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.scaler.util
 
 import android.widget.ImageView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import com.example.scaler.R
 import com.example.scaler.constants.APIConstants
@@ -19,4 +20,9 @@ object BaseBindingAdapter {
     fun loadThumbnailImage(view: ImageView?, url: String?) {
         view?.loadImage(APIConstants.URL_IMAGE_PREFIX + url, R.drawable.ic_placeholder_image)
     }
+//    @BindingAdapter("changeBGColor")
+//    @JvmStatic
+//    fun loadThumbnailImage(clMainVideoView:ConstraintLayout) {
+//
+//    }
 }
